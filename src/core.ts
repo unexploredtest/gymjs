acb2a2c563020d80b6bd6995197ae48e7c96e976
unexploredtest <53617231+unexploredtest@users.noreply.github.com>
2025-09-27 13:31:41 +0000

    Add Env (#10)
    * Add base class Env
    
    * Make infotype generic
    
    * Move any from default for InfoType to where it's used
    
    * Use generics to specify info type
    
    * Put export in front of types instad of the end file

---
+++ src/core.ts
@@ -0,0 +1,26 @@
+import * as tf from '@tensorflow/tfjs';
+
+import { Discrete } from './spaces/discrete';
+import { Box } from './spaces/box';
+
+type ActSpace = Discrete;
+type ObsSpace = Box;
+
+export type InfoType<T> = Record<string, T>;
+
+export abstract class Env<T> {
+    protected renderMode: string;
+    public actionSpace: ActSpace;
+    public observationSpace: ObsSpace;
+
+    constructor(actionSpace: ActSpace, observationSpace: ObsSpace, renderMode: string) {
+        this.actionSpace = actionSpace;
+        this.observationSpace = observationSpace;
+        this.renderMode = renderMode;
+    }
+
+    abstract reset(): [tf.Tensor, InfoType<T> | null];
+    abstract step(action: tf.Tensor | number): Promise<[tf.Tensor, number, boolean, boolean, InfoType<T> | null]>; // Action is number for now
+    abstract render(): Promise<void | tf.Tensor>;
+    abstract close(): void;
+}
