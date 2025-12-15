547f820e5b5fefcdf4acec39d2afdc617dafee73
unexploredtest <unexploredtest@tutanota.com>
2025-12-15 23:23:40 +0330

    Spaces: Add multi-binary

--- src/spaces/index.ts
+++ src/spaces/index.ts
@@ -2,4 +2,5 @@ import { Space } from './space';
 import { Discrete } from './discrete';
 import { Box } from './box';
 import { MultiDiscrete } from './multidiscrete';
-export { Space, Discrete, Box, MultiDiscrete };
+import { MultiBinary } from './multibinary';
+export { Space, Discrete, Box, MultiDiscrete, MultiBinary };
